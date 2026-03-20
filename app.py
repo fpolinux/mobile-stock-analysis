20544a844a60fe664202d1aef16321f84d97b980
Narayan Shastri <fpodunedin83@gmail.com>
2026-03-20 14:22:00 +1300

    Flask Ticker Input Handling

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def home():
-    return "Flask is working"
+    result = None
+
+    if request.method == "POST":
+        ticker = request.form.get("ticker","").upper().strip()
+        result = f"Running analysis for {ticker}"
+    
+    return render_template("index.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
